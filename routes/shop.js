9c09d6c7f41a27c280d79df8e7927cc2af8bb6f1
annusolanki1998 <annusinghraj1998@gmail.com>
2022-02-02 22:58:35 +0530

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+//const rootDir = require('../util/path');
+
+const productsController = require('../controllers/products'); 
+
+//const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', productsController.getProducts); 
+
+module.exports = router;
